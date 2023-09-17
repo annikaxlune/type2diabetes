2aef677e5e697eb9f8f6243e179bd81b8465f0f0
Mousom Mondal <mousom.mondal@gmail.com>
2023-09-17 18:55:20 -0500

    Lotta changes - including help topic, picture

--- main.py
+++ main.py
@@ -6,27 +6,42 @@ from glob import glob
 import streamlit as st
 from pathlib import Path
 
-columnColor = "orange"
+columnColor = 'orange'
+helpColor = 'violet'
 
+
+# [data-TestId = "baseButton-header"] {
+# visibility: hidden
+# }
+# [data - testId = "stAppViewContainer"]
+# background-image: url("data:image/png;base64,%s");
+# background-image: url("https://type2-diabetes-images.s3.us-west-2.amazonaws.com/flat-lay-doodle-template-for-diabetes-concept-black-and-white-vector-illustration-top-view-2DADTBR.jpg")
 
 def add_bg_from_url():
     background_img = """
     <style>
-            [data-testid="stAppViewContainer"]{
+        [data-testId = "stHeader"] {
+        background-color: rgb(191, 87, 0)
+        }
+    body
+        {
             background-image: url("data:image/png;base64,%s");
             background-size: cover;
+            opacity: .95
         }
-        [data-testid="stHeader"]{
-        background-color: rgb(191, 87, 0);
-        text : "Hello"
+        .overlay {
         }
     </style>
-    """ % img_to_bytes("images/pexels-nataliya-vaitkevich-6941883.jpg");
+    """ % img_to_bytes(
+        "images/flat-lay-doodle-template-for-diabetes-concept-black-and-white-vector-illustration-top-view-2DADTBR.jpg")
+
     st.markdown(
         background_img,
         unsafe_allow_html=True
     )
-    st.markdown("""[data - testid = "stHeader"][Hello!!]""",unsafe_allow_html=True)
+    return
+    # Overlay the dimming effect
+    # st.markdown('<div class="overlay"></div>', unsafe_allow_html=True)
 
 
 def get_ageGrp(age):
@@ -51,7 +66,7 @@ def img_to_bytes(img_path):
 
 
 def get_trigLevel(param):
-    return "No" if param < 199 else "Yes"  # clarify - what qualifies Yes
+    return "No" if param < 150 else "Yes"  # clarify - what qualifies Yes
 
 
 def get_glucoseLevel(param):
@@ -61,7 +76,7 @@ def get_glucoseLevel(param):
 def get_bmiCat(param):
     if param < 18.5:
         return "Underweight: Less than 18.5"
-    elif param > 18.5 and param > 24.9:
+    elif 18.5 < param < 24.9:
         return "Optimum range: 18.5 to 24.9"
     elif param > 25 and param > 29.9:
         return "Overweight: 25 to 29.9"
@@ -73,8 +88,8 @@ def get_bmiCat(param):
         return "Class III obesity: More than 40"
 
 
-def get_ht_ind(param):
-    return "Yes" if param > 140 else "No"
+def get_ht_ind(sp, dp):
+    return "Yes" if sp > 120 or dp > 80 else "No"
 
 
 def header(url):
@@ -138,56 +153,105 @@ class Diabetes(object):
                          "Class II obesity: 35 to 39.9": 1.0161,
                          "Class III obesity: More than 40": 1.231
                          }
+        self.bmi_calculated = 0
         # st.image('images/dia1.jpg')
 
+    def display_bmi(self):
+        bmi_color = ":green" if self.bmi_calculated >= 18.5 and self.bmi_calculated <= 24.9 else ":red"
+        st.markdown(f"{bmi_color}[*Your BMI :{round(self.bmi_calculated, 1)}*]",
+                    help=f":{helpColor}[*Body Mass Index (BMI) is a "
+                         f"person’s "
+                         "weight in "
+                         "kilograms (or pounds) divided by the square of height in meters (or feet). A high BMI can indicate high body fatness. BMI screens for weight categories that may lead to health problems, but it does not diagnose the body fatness or health of an individual. For more refer to https://www.cdc.gov/healthyweight/assessing/bmi/index.html#:~:text=Body%20Mass%20Index%20(BMI)%20is,or%20health%20of%20an%20individual.*]")
+
     def set_columns(self):
-        st.header("Press Submit after answering the below questions :")
+        # st.header("Press Submit after answering the below questions :")
+        st.markdown("<h4 style='color: rgb(191, 87, 0);'>Press Submit after "
+                    "answering the below questions :</h4>", unsafe_allow_html=True)
         col1, col2, col3 = st.columns(3)
         with col1:
             # st.write("Demographic information")
-            self.race = float(self.race_dict[st.radio(f":{columnColor}[**Your race:**]", tuple(self.race_dict.keys()),
-                                                      help=f":{columnColor}[**Race**]",
+            self.race = float(self.race_dict[st.radio(label=f":{columnColor}[**Your race:**]",
+                                                      options=tuple(self.race_dict.keys()),
+                                                      help=f":{helpColor}[*Race is a person's self-identification "
+                                                           f"with one or more social groups. According to census "
+                                                           f"surveys, "
+                                                           f"an individual can report as White, Black or African "
+                                                           f"American, Asian, American Indian and Alaska Native, "
+                                                           f"Native Hawaiian and Other Pacific Islander, "
+                                                           f"or some other race. For more refer to https://www.census.gov/topics/population/race/about.html*]",
                                                       )])
             self.gender = float(self.sex_dict[st.radio(f":{columnColor}[**Your gender:**]",
                                                        tuple(self.sex_dict.keys()),
-                                                       help=f":{columnColor}[**Enter your biological gender:**]")])
+                                                       help=f":{helpColor}[*Sex assigned at birth – The sex "
+                                                            f"(male or female) assigned to an infant, most often "
+                                                            f"based on the infant's anatomical and other biological "
+                                                            f"characteristics. Sometimes referred to as birth sex, "
+                                                            f"natal sex, biological sex or sex. For more refer to https://www.uwmedicine.org/practitioner-resource/lgbtq/lgbtq-inclusion-glossary#:~:text=Sex%20assigned%20at%20birth%20(noun,birth%20is%20the%20recommended%20term.*]")])
             # self.age_grp = float(self.age_grp[st.radio("Your age group:", tuple(self.age_grp.keys()))])
-            c1, c2, c3 = st.columns(3)
-            with c1:
-                self.age_grp = float(self.age_grp[get_ageGrp(st.number_input(label=f":{columnColor}[**Your age:**]",
-                                                                             value=35, min_value=1,
-                                                                             max_value=100, step=1))])
+            # c1, c2, c3 = st.columns(3)
+            # with c1:
+            self.age_grp = float(self.age_grp[get_ageGrp(st.number_input(label=f":{columnColor}[**Your age:**]",
+                                                                         value=35, min_value=1,
+                                                                         max_value=100, step=1))])
 
         # self.hypertension = float(self.ht_dict[st.radio("Blood pressure over 140 mmHg ?",
         #                                                         tuple(self.ht_dict.keys()))])
         with col2:
-            c1, c2, c3 = st.columns(3)
-            with c1:
-                self.LDL_level = float(self.ldl_dict["Yes" if (st.slider(label=f":{columnColor}[**Your LDL (mg/dL) "
-                                                                               "?:**]", min_value=0,
-                                                                         max_value=250)) < 150 else "No"])
-                self.hypertension = float(
-                    self.ht_dict[get_ht_ind((st.number_input(f":{columnColor}[**Enter your blood pressure level ("
-                                                             "mmHg):**] ",
-                                                             value=120, min_value=0, max_value=500)))])
+            # c1, c2, c3 = st.columns(3)
+            # with c1:
+            self.LDL_level = float(self.ldl_dict["Yes" if (st.slider(label=f":{columnColor}[**Your LDL (mg/dL)"
+                                                                           ":**]", min_value=0,
+                                                                     help=f":{helpColor}[*LDL (low-density "
+                                                                          f"lipoprotein) cholesterol, "
+                                                                          f"sometimes called “bad” cholesterol.*]",
+                                                                     max_value=250)) < 150 else "No"])
 
-                self.trig = float(self.trig_dict[get_trigLevel(st.number_input(f":{columnColor}[**Enter "
-                                                                               "triglycerides:**]", value=90))])
+            sp = (st.number_input(f":{columnColor}[**Your Systolic blood pressure ("
+                                  f"first number) :**] ",
+                                  help=f":{helpColor}[Systolic blood pressure (the first number) – indicates how much pressure your blood is exerting against your artery walls when the heart contracts.]",
+                                  value=120, min_value=0, max_value=500))
+            dp = (st.number_input(f":{columnColor}[**Your Diastolic blood pressure ("
+                                  f"second number) :**] ",
+                                  help=f":{helpColor}[Diastolic blood pressure (the second number) – indicates how much pressure your blood is exerting against your artery walls while the heart muscle is resting between contractions.]",
+                                  value=60, min_value=0, max_value=100))
+            self.hypertension = float(
+                self.ht_dict[get_ht_ind(sp, dp)])
 
         with col3:
-            c1, c2, c3 = st.columns(3)
-            with c1:
-                self.hglycemia = float(self.hg_dict[get_glucoseLevel(
-                    st.number_input(f":{columnColor}[**Glucose level per latest test :**]",
-                                    min_value=90,
-                                    max_value=400))])
-                self.height = float(
-                    st.number_input(f":{columnColor}[**Enter your height in feet:**]", min_value=2.0, max_value=10.0,
-                                    step=.25))
-                self.weight = float(st.number_input(f":{columnColor}[**Enter your weight in LBs:**]", min_value=10.0,
-                                                    max_value=600.0,
-                                                    step=.5))
-                self.bmi = self.bmi_dict[get_bmiCat(703 * self.weight / ((self.height * 12) ** 2))]
+            # c1, c2, c3 = st.columns(3)
+            # with c1:
+            self.trig = float(self.trig_dict[get_trigLevel(st.number_input(f":{columnColor}[**Your "
+                                                                           "triglycerides:**]",
+                                                                           help=f":{helpColor}[*Triglycerides are the most common form of fat in the bloodstream. They consist of three fatty acid chains linked by a molecule called glycerol. For more refer to https://www.health.harvard.edu/heart-health/understanding-triglycerides*]",
+                                                                           value=90))])
+            self.hglycemia = float(self.hg_dict[get_glucoseLevel(
+                st.number_input(f":{columnColor}[**Glucose level :**]",
+                                help=f":{helpColor}[*The blood sugar level, blood sugar concentration, blood glucose "
+                                     f"level, "
+                                     f"or glycemia, is the measure of glucose concentrated in the blood. The body "
+                                     f"tightly regulates blood glucose levels as a part of metabolic homeostasis.[2] "
+                                     f"For more refer to https://en.wikipedia.org/wiki/Blood_sugar_level"
+                                     f"* ]",
+                                min_value=90,
+                                max_value=400))])
+            self.height = float(
+                st.number_input(f":{columnColor}[**Enter your height in feet:**]", min_value=2.0, max_value=10.0,
+                                step=.25))
+            self.weight = float(st.number_input(f":{columnColor}[**Enter your weight in LBs:**]", min_value=10.0,
+                                                max_value=600.0,
+                                                step=.5))
+            bmi_cat = 703 * self.weight / ((self.height * 12) ** 2)
+            bmi_color = ":green" if bmi_cat >= 18.5 and bmi_cat <= 24.9 else ":red"
+            self.bmi_calculated = bmi_cat
+            # self.display_bmi()
+            # st.markdown(f"{bmi_color}[*Your BMI :{round(bmi_cat, 1)}*]",
+            #             help=f":{helpColor}[*Body Mass Index (BMI) is a "
+            #                  f"person’s "
+            #                  "weight in "
+            #                  "kilograms (or pounds) divided by the square of height in meters (or feet). A high BMI can indicate high body fatness. BMI screens for weight categories that may lead to health problems, but it does not diagnose the body fatness or health of an individual. For more refer to https://www.cdc.gov/healthyweight/assessing/bmi/index.html#:~:text=Body%20Mass%20Index%20(BMI)%20is,or%20health%20of%20an%20individual.*]")
+            # st.write(bmi_cat)
+            self.bmi = self.bmi_dict[get_bmiCat(bmi_cat)]
 
     def calc_probability(self):
         s = self.intercept + self.race + self.gender + self.age_grp + self.LDL_level + self.hypertension + self.trig + self.hglycemia \
@@ -217,11 +281,15 @@ class Diabetes(object):
         st.markdown("""---""")
 
     def set_title_header(self):
-        c1, c2 = st.columns(2)
+        c1, c2 = st.columns([9, 1])
         with c1:
-            st.markdown("<h2 style='text-align: right; color: rgb(191, 87, 0);'>PREDICTIVE MODEL FOR CALCULATING "
-                        "INDIVIDUAL'S RISK OF TYPE 2 DIABETES</h2>",
+            st.markdown("<h2 style='text-align: right; color: rgb(191, 87, 0);'>Predictive Model For Calculating "
+                        "Risk Of Type 2 Diabetes </h2>",
                         unsafe_allow_html=True)
+            st.markdown(
+                "<p style='text-align: right; color: rgb(191, 87, 0); font-style: italic'>- Mondal, Annika and Dr. Truong Chau, "
+                "Ph.D. UTHealth School of Public Health, Houston, TX, USA</p>",
+                unsafe_allow_html=True)
         with c2:
             st.markdown(
                 '''[<img src='data:image/png;base64,{}' class='img-fluid' width=128 height=128 style="float:right">](
@@ -230,16 +298,6 @@ class Diabetes(object):
 
 
 if __name__ == '__main__':
-    # streamlit_style = """
-    # 			<style>
-    # 			@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
-    # 			html, body, [class*="css"]  {
-    # 			font-family: 'Roboto', sans-serif;
-    # 			}
-    # 			</style>
-    # 			"""
-    # st.markdown(streamlit_style, unsafe_allow_html=True)
-    # st.set_page_config(layout="wide")
     st.set_page_config(
         page_title='Type 2 Diabetes',
         layout="wide",
@@ -250,21 +308,31 @@ if __name__ == '__main__':
             [data-testid="stForm"] {border: 0px}
         </style>
     '''
-    # add_bg_from_url()
+    add_bg_from_url()
 
     st.markdown(css, unsafe_allow_html=True)
     diabetes = Diabetes()
     diabetes.set_title_header()
 
-    expander = st.expander(label=f"**Find out probability of having diabetes**",expanded=True)
+    expander = st.expander(label=f"**Find out probability of having diabetes**", expanded=True)
     with expander:
         with st.form(key="my_form"):
             diabetes.set_columns()
             submit_button = st.form_submit_button(label='Submit', on_click=diabetes.calc_probability())
-            # write(submit_button)
-        write(f"The probability of having type 2 diabetes : {round(diabetes.result, 2)}%", "green" if float(
-            diabetes.result) < .5 else
-        "red")
+        diabetes.display_bmi()
+        result_text_color = "green" if float(diabetes.result) < 50 else "red"
+        st.markdown(f":{result_text_color}[**The probability of having type 2 diabetes : "
+                    f"{round(diabetes.result, 2)}**]")
+        st.markdown("<h4 style='text-align: left; color: rgb(191, 87, 0); font-style: normal'>Check below your "
+                    "personalized recommendation to manage your condition.</h4>",
+                    unsafe_allow_html=True)
+
+        tab1, tab2 = st.tabs(['DOs', 'DONTs'])
+        with tab1:
+            st.markdown("<h4 style='color: black'>More of this</h4>",unsafe_allow_html=True)
+
+        with tab2:
+            st.markdown("<h4 style='color: black'>Less of this</h4>",unsafe_allow_html=True)
 
     # with diabetes.tab3:
     with st.expander(f"**Project Detail**"):
